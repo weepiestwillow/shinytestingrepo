31ee9dc547b1e63b3ee3653a9ce0bafe9e014081
Willow Smith <exrobite@gmail.com>
2026-07-14 17:47:23 +0100

    book excercises

--- Dissertation Project 2/book testing.R
+++ Dissertation Project 2/book testing.R
@@ -8,6 +8,16 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
+  dataset <- reactive({
+    get(input$dataset, "package:datasets")
+  })
+  
+  output$summary <- renderPrint({
+    summary(dataset())
+  })
+  output$table <- renderTable({
+    dataset()
+  })
 }
 
 shinyApp(ui, server)
---
+++ Dissertation Project 2/multiplication sliders.R
@@ -0,0 +1,14 @@
+ui <- fluidPage(
+  sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
+  sliderInput("y", label = "and y is", min = 1, max = 50, value = 30),
+  "then x times y is",
+  textOutput("product")
+)
+
+server <- function(input, output, session) {
+  output$product <- renderText({ 
+    input$x * input$y
+  })
+}
+
+shinyApp(ui, server)
